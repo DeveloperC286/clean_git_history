03c29960d8b8ba5dcce8bd85646808ecc1b3c261
DeveloperC <DeveloperC@protonmail.com>
2021-09-30 19:05:58 +0100

    refactor: parse_to_oid returns Result

--- src/model/commits/mod.rs
+++ src/model/commits/mod.rs
@@ -1,5 +1,3 @@
-use std::process::exit;
-
 use git2::{Oid, Repository, Revwalk};
 
 use crate::model::commits::commit::Commit;
@@ -20,7 +18,8 @@ impl Commits {
 
     pub(crate) fn from_commit_hash(from_commit_hash: String) -> Result<Self, git2::Error> {
         let repository = get_repository()?;
-        get_commits_till_head_from_oid(&repository, parse_to_oid(&repository, from_commit_hash))
+        let commit_oid = parse_to_oid(&repository, from_commit_hash)?;
+        get_commits_till_head_from_oid(&repository, commit_oid)
     }
 
     pub(crate) fn contains_merge_commits(&self) -> bool {
@@ -100,80 +99,62 @@ fn get_reference_oid(repository: &Repository, matching: &str) -> Result<Oid, git
     }
 }
 
-fn parse_to_oid(repository: &Repository, oid: String) -> Oid {
+fn parse_to_oid(repository: &Repository, oid: String) -> Result<Oid, git2::Error> {
     match oid.len() {
-        0 => {
-            error!("Provided Git commit hash is empty and can not be parsed.");
-            exit(crate::ERROR_EXIT_CODE);
-        }
         1..=39 => {
             trace!(
-                "Attempting to find a match for the short commit hash {:?}",
+                "Attempting to find a match for the short commit hash {:?}.",
                 oid
             );
             let matching_oid_lowercase = oid.to_lowercase();
 
-            match repository.revwalk() {
-                Ok(mut revwalk) => {
-                    match revwalk.push_head() {
-                        Ok(_) => {}
-                        Err(_) => {
-                            error!("Unable to push HEAD onto the Git revision walker.");
-                            exit(crate::ERROR_EXIT_CODE);
+            let mut revwalker = repository.revwalk()?;
+            revwalker.push_head()?;
+
+            let matched_commit_hashes: Vec<Oid> = revwalker
+                .into_iter()
+                .map(|result| {
+                    return match result {
+                        Ok(oid) => {
+                            let oid_lowercase = oid.to_string().to_lowercase();
+
+                            if oid_lowercase.starts_with(&matching_oid_lowercase) {
+                                return Some(oid);
+                            }
+
+                            None
                         }
-                    }
+                        Err(error) => {
+                            error!("{:?}", error);
 
-                    let matched_commit_hashes: Vec<Oid> = revwalk
-                        .into_iter()
-                        .map(|result| {
-                            return match result {
-                                Ok(oid) => {
-                                    let oid_lowercase = oid.to_string().to_lowercase();
-
-                                    if oid_lowercase.starts_with(&matching_oid_lowercase) {
-                                        return Some(oid);
-                                    }
-
-                                    None
-                                }
-                                Err(error) => {
-                                    error!("{:?}", error);
-
-                                    None
-                                }
-                            };
-                        })
-                        .flatten()
-                        .collect();
-
-                    match matched_commit_hashes.len() {
-                        0 => {
-                            error!("No actual commit hashes start with the provided short commit hash {:?}.", matching_oid_lowercase);
-                            exit(crate::ERROR_EXIT_CODE);
+                            None
                         }
-                        1 => *matched_commit_hashes.first().unwrap(),
-                        _ => {
-                            error!("Ambiguous short commit hash, the commit hashes {:?} all start with the provided short commit hash {:?}.", matched_commit_hashes, matching_oid_lowercase);
-                            exit(crate::ERROR_EXIT_CODE);
-                        }
-                    }
+                    };
+                })
+                .flatten()
+                .collect();
+
+            match matched_commit_hashes.len() {
+                0 => {
+                    error!(
+                        "No actual commit hashes start with the provided short commit hash {:?}.",
+                        matching_oid_lowercase
+                    );
+                    Err(git2::Error::from_str(""))
                 }
-                Err(error) => {
-                    error!("{:?}", error);
-                    exit(crate::ERROR_EXIT_CODE);
+                1 => Ok(*matched_commit_hashes.first().unwrap()),
+                _ => {
+                    error!("Ambiguous short commit hash, the commit hashes {:?} all start with the provided short commit hash {:?}.", matched_commit_hashes, matching_oid_lowercase);
+                    Err(git2::Error::from_str(""))
                 }
             }
         }
-        40 => match git2::Oid::from_str(&oid) {
-            Ok(oid) => oid,
+        _ => match git2::Oid::from_str(&oid) {
+            Ok(oid) => Ok(oid),
             Err(error) => {
-                error!("{:?}", error);
-                exit(crate::ERROR_EXIT_CODE);
+                error!("{:?} is not a valid commit hash.", oid);
+                Err(error)
             }
         },
-        _ => {
-            error!("Provided Git commit hash is too long and can not be parsed.");
-            exit(crate::ERROR_EXIT_CODE);
-        }
     }
 }
