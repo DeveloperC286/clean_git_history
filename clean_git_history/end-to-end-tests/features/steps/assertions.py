897b534626a707e1bf79e32e6578f202c9547114
DeveloperC <DeveloperC@protonmail.com>
2022-12-06 22:16:42 +0000

    test: converting to assert_empty

--- clean_git_history/end-to-end-tests/features/steps/assertions.py
+++ clean_git_history/end-to-end-tests/features/steps/assertions.py
@@ -4,3 +4,7 @@ def assert_successful(exit_code):
 
 def assert_unsuccessful(exit_code):
     assert exit_code != 0, f"Expected a non-zero exit code to indicate a unsuccessful execution, got '{exit_code}'."
+
+
+def assert_empty(output):
+    assert output == "", f"Expected the output to be empty, got:\n{output.encode()}"
--- clean_git_history/end-to-end-tests/features/steps/then.py
+++ clean_git_history/end-to-end-tests/features/steps/then.py
@@ -2,7 +2,7 @@ import re
 from behave import then
 
 from utilities import execute_clean_git_history
-from assertions import assert_successful, assert_unsuccessful
+from assertions import assert_successful, assert_unsuccessful, assert_empty
 
 
 @then('the Git history is clean.')
@@ -11,8 +11,8 @@ def git_history_is_clean(context):
     execute_clean_git_history(context)
 
     # Then
-    assert context.stdout == ""
-    assert context.stderr == ""
+    assert_empty(context.stdout)
+    assert_empty(context.stderr)
     assert_successful(int(context.exit_code))
 
 
@@ -22,7 +22,7 @@ def git_history_is_not_clean(context):
     execute_clean_git_history(context)
 
     # Then
-    assert context.stdout == ""
+    assert_empty(context.stdout)
     assert_unsuccessful(int(context.exit_code))
 
 
