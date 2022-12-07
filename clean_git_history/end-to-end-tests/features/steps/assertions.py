d2fa1e1dbccc88f233fb032359e7ed1f2d4484e3
DeveloperC <DeveloperC@protonmail.com>
2022-12-07 18:29:40 +0000

    refactor: improved assertion error messages

--- clean_git_history/end-to-end-tests/features/steps/assertions.py
+++ clean_git_history/end-to-end-tests/features/steps/assertions.py
@@ -1,10 +1,11 @@
 def assert_successful(exit_code):
-    assert exit_code == 0, f"Expected a zero exit code to indicate a successful execution, got '{exit_code}'."
+    assert exit_code == 0, f"Expected a zero exit code to indicate a successful execution.\nExit code = '{exit_code}'."
 
 
 def assert_unsuccessful(exit_code):
-    assert exit_code != 0, f"Expected a non-zero exit code to indicate a unsuccessful execution, got '{exit_code}'."
+    assert exit_code != 0, f"Expected a non-zero exit code to indicate a unsuccessful execution\nExit code = '{exit_code}'."
 
 
 def assert_empty(output):
-    assert output == "", f"Expected the output to be empty, got:\n{output.encode()}"
+    assert output == "", f"Expected the output to be empty.\nOutput = {output.encode()}."
+
