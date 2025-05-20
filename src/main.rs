0b9021509338519e0c9bf74c7af6b79d753bb5a0
DeveloperC <DeveloperC@protonmail.com>
2025-05-20 22:34:59 +0100

    refactor: max_commits is now optional (#213)

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,31 @@ use anyhow::{bail, Context, Result};
 use clap::Parser;
 use git2::Repository;
 
-mod cli;
 mod commits;
 
-use crate::cli::Arguments;
 use crate::commits::Commits;
 
 const ERROR_EXIT_CODE: i32 = 1;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub(crate) struct Arguments {
+    #[arg(
+        long,
+        help = "The maximum number of commits allowed, if exceeded will cause linting to fail."
+    )]
+    pub(crate) max_commits: Option<usize>,
+
+    #[arg(
+        help = "The Git reference from where to start taking the range of commits from till HEAD to lint. The range is inclusive of HEAD and exclusive of the provided reference."
+    )]
+    pub(crate) from: String,
+}
+
 fn main() {
     pretty_env_logger::init();
     trace!("Version {}.", env!("CARGO_PKG_VERSION"));
-    let arguments = cli::Arguments::parse();
+    let arguments = Arguments::parse();
     debug!("The command line arguments provided are {:?}.", arguments);
 
     if let Err(err) = run(arguments) {
@@ -34,12 +47,14 @@ fn run(arguments: Arguments) -> Result<()> {
         bail!("Contains merge commits.");
     }
 
-    if arguments.max_commits > 0 && commits.len() > arguments.max_commits {
-        bail!(format!(
-            "Exceeded the maxium number of commits {:?} with {:?} commits.",
-            arguments.max_commits,
-            commits.len()
-        ));
+    if let Some(max_commits) = arguments.max_commits {
+        if commits.len() > max_commits {
+            bail!(format!(
+                "Exceeded the maxium number of commits {:?} with {:?} commits.",
+                arguments.max_commits,
+                commits.len()
+            ));
+        }
     }
 
     Ok(())
