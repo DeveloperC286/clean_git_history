ae19a7db693e2a9f70cc9b835e07788f68782e53
DeveloperC <DeveloperC@protonmail.com>
2025-07-14 13:38:58 +0100

    docs: condensed/simplified README.md (#274)

--- Makefile
+++ Makefile
@@ -108,7 +108,7 @@ publish-crate:
 .PHONY: dogfood-docker
 dogfood-docker: release
 	docker build -t clean_git_history -f Dockerfile .
-	docker run $(DOCKER_RUN_WRITE_OPTS) clean_git_history --verbose
+	docker run $(DOCKER_RUN_WRITE_OPTS) clean_git_history --verbose $(FROM)
 
 .PHONY: publish-docker
 publish-docker: release
--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,7 @@ pub(crate) struct Arguments {
 }
 
 fn main() {
-    info!("Version {}.", env!("CARGO_PKG_VERSION"));
     let arguments = Arguments::parse();
-    debug!("The command line arguments provided are {arguments:?}.");
 
     // Set up logging: if verbose is true and RUST_LOG is not set, default to info level
     if arguments.verbose && std::env::var("RUST_LOG").is_err() {
@@ -45,6 +43,9 @@ fn main() {
     }
 
     pretty_env_logger::init();
+
+    info!("Version {}.", env!("CARGO_PKG_VERSION"));
+    debug!("The command line arguments provided are {arguments:?}.");
 
     if let Err(err) = run(arguments) {
         error!("{err:?}");
